c4a9115785dd5dfb9dc122d2f21ceb81581ebde7
Charlotte <70633528+CharlotteH86@users.noreply.github.com>
2024-04-14 12:43:25 +0200

    Initial Commit

---
+++ Emoji/EmojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiApp.swift
+//  Emoji
+//
+//  Created by Charlotte Holst on 2024-04-14.
+//
+
+import SwiftUI
+
+@main
+struct EmojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
